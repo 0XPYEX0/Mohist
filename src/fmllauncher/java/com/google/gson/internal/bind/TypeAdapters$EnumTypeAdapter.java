e104697184b940d4a81226a1cdc0531485ae1606
0XPYEX0 <50171612+0XPYEX0@users.noreply.github.com>
2021-04-24 18:23:21 +0800

    Create TypeAdapters$EnumTypeAdapter.java

---
+++ src/fmllauncher/java/com/google/gson/internal/bind/TypeAdapters$EnumTypeAdapter.java
@@ -0,0 +1,47 @@
+package com.google.gson.internal.bind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public final class TypeAdapters$EnumTypeAdapter<T extends Enum<T>> extends TypeAdapter<T> {
+    private final Map<T, String> cn = new HashMap<T, String>();
+    private final Map<String, T> nc = new HashMap<String, T>();
+
+    public TypeAdapters$EnumTypeAdapter(Class<T> classOfT) {
+        for (T constant : classOfT.getEnumConstants()) {
+            String name = constant.name();
+
+            try {
+                SerializedName annotation = classOfT.getField(name).getAnnotation(SerializedName.class);
+                if (annotation == null) {
+                    return;
+                }
+                name = annotation.value();
+                for (String alternate : annotation.alternate()) {
+                    nc.put(alternate, constant);
+                }
+            } catch (Exception e) {}
+
+            nc.put(name, constant);
+            cn.put(constant, name);
+        }
+    }
+    @Override
+    public void write(JsonWriter out, T value) throws Exception {
+        out.value(value == null ? null : constantToName.get(value));
+    }
+    @Override
+    public T read(JsonReader in) throws Exception {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+        return nameToConstant.get(in.nextString());
+    }
+} 
